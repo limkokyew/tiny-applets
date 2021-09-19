6f8e5e617628309b50ba456bb422fad32f265aab
Kok Yew Lim <lim.kokyew16@gmail.com>
2021-09-19 14:23:40 +0200

    Updated coverage checker results table and type chart

--- pkmn-coverage-checker/coverage-checker.js
+++ pkmn-coverage-checker/coverage-checker.js
@@ -1,7 +1,85 @@
+// TODO: Can this be implemented any better?
+// Ideally, this would be on another file, but because of CORS, this
+// workaround is needed
+const typeChart = {
+  "Normal": {
+    0.5: ["Ghost", "Rock", "Steel"],
+    2: []
+  },
+  "Fighting": {
+    0.5: ["Ghost", "Flying", "Poison", "Bug", "Psychic", "Fairy"],
+    2: ["Normal", "Rock", "Steel", "Ice", "Dark"]
+  },
+  "Flying": {
+    0.5: ["Rock", "Steel", "Electric"],
+    2: ["Fighting", "Bug", "Grass"]
+  },
+  "Poison": {
+    0.5: ["Steel", "Poison", "Ground", "Rock", "Ghost"],
+    2: ["Grass", "Fairy"]
+  },
+  "Ground": {
+    0.5: ["Flying", "Bug", "Grass"],
+    2: ["Poison", "Rock", "Steel", "Fire", "Electric"]
+  },
+  "Rock": {
+    0.5: ["Fighting", "Ground", "Steel"],
+    2: ["Flying", "Bug", "Fire", "Ice"]
+  },
+  "Bug": {
+    0.5: ["Fighting", "Flying", "Poison", "Ghost", "Steel", "Fire", "Fairy"],
+    2: ["Grass", "Psychic", "Dark"]
+  },
+  "Ghost": {
+    0.5: ["Normal", "Dark"],
+    2: ["Psychic", "Ghost"]
+  },
+  "Steel": {
+    0.5: ["Steel", "Fire", "Water", "Electric"],
+    2: ["Rock", "Ice", "Fairy"]
+  },
+  "Fire": {
+    0.5: ["Rock", "Fire", "Water", "Dragon"],
+    2: ["Bug", "Steel", "Grass", "Ice"]
+  },
+  "Water": {
+    0.5: ["Water", "Grass", "Dragon"],
+    2: ["Ground", "Rock", "Fire"]
+  },
+  "Grass": {
+    0.5: ["Flying", "Poison", "Bug", "Steel", "Fire", "Grass", "Dragon"],
+    2: ["Ground", "Rock", "Water"]
+  },
+  "Electric": {
+    0.5: ["Ground", "Grass", "Electric", "Dragon"],
+    2: ["Flying", "Water"]
+  },
+  "Psychic": {
+    0.5: ["Dark", "Steel", "Psychic"],
+    2: ["Fighting", "Poison"]
+  },
+  "Ice": {
+    0.5: ["Steel", "Fire", "Water", "Ice"],
+    2: ["Flying", "Ground", "Grass", "Dragon"]
+  },
+  "Dragon": {
+    0.5: ["Fairy", "Steel"],
+    2: ["Dragon"]
+  },
+  "Dark": {
+    0.5: ["Fighting", "Dark", "Fairy"],
+    2: ["Ghost", "Psychic"]
+  },
+  "Fairy": {
+    0.5: ["Poison", "Fire", "Steel"],
+    2: ["Fighting", "Dragon", "Dark"]
+  }
+}
+
 // -----
 // Globals and such
 // -----
-const typeClassname = "type";
+const typeClassname = "type-draggable";
 const typeArea = document.getElementById("type-selection-box");
 const typeRect = typeArea.getBoundingClientRect();
 
@@ -75,8 +153,6 @@ function divMove(e) {
   div.style.top = (e.clientY - yOffset) + "px";
   div.style.left = (e.clientX - xOffset) + "px";
   
-  /* if (e.clientX >= dropRect.left && e.clientX <= dropRect.right
-   && e.clientY >= dropRect.top && e.clientY <= dropRect.bottom) { */
   if (isInDropRect(e.clientX, e.clientY)) {
     dropArea.classList.add(dropAreaHighlightClassName);
   } else {
