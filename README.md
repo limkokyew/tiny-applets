0582c2b897137efcf5f72021f817a037e0b1880c
Kok Yew Lim <lim.kokyew16@gmail.com>
2021-09-29 16:15:53 +0200

    Added initial version of new applet, breakout

--- README.md
+++ README.md
@@ -1,40 +1,60 @@
 # Tiny applets
-A personal collection of small applets and command line tools that do not
+A personal collection of small applets and / or command line tools that do not
 warrant their own repository. I implemented these primarily with Python and
-JavaScript, but I made use of other programming languages and / or technologies
-as well. Refer to the ``README`` of each individual applet for more information.
+JavaScript, but I occasionally made use of other programming languages and / or
+technologies as well. Refer to the ``README`` of each individual applet for more
+information.
 
 ## List of applets
 
 ### 🎓 ECTS grade calculator
-**Technologies used**: HTML, CSS, JavaScript
+**Languages used**: HTML, CSS, JavaScript
+
+**Libraries used**: None
 
 Want to see how well you've done in college? Enter your grades into the
 calculator and it'll give you your final grade.
 
 ### 📝 Study helper
-**Technologies used**: Python
+**Languages used**: Python
+
+**Libraries used**: None
 
 Particularly useful for studying. Import a text document with your notes and
 the study helper will ask you questions based on what you've written in your
 notes.
 
 ### 🎇 Pokémon coverage checker
-**Technologies used**: HTML, CSS, JavaScript
+**Languages used**: HTML, CSS, JavaScript
 
-Check how your team's coverage fares against all Pokémon types. May be useful
-for teambuilding.
+**Libraries used**: None
+
+Check how your team's coverage fares against all Pokémon types. Useful
+for building your next competitive team, whether it is for post-game battle
+facilities or ranked online matches!
 
 ### 🕔 Timer app
-**Technologies used**: Python (PyQt5)
+**Languages used**: Python
+
+**Libraries used**: ``pyqt5``
 
 Where does the time go? Track how much time you spend on different activites,
 such as studying or relaxing, and if necessary, adjust your schedule
 accordingly!
 
 ### 🐍 Snake game
-**Technologies used**: Python
+**Languages used**: Python
+
+**Libraries used**: ``asciimatics``, ``pynput``
 
 A classic game that probably everyone has played at least once. Control a snake
 and eat the surrounding fruit, but avoid going out of bounds or ... hitting
 yourself!
+
+### ⬜ Breakout
+**Technologies used**: Python
+
+**Libraries used**: ``asciimatics``, ``pynput``
+
+Yet another classic and equally as popular game. Take control of a paddle and
+bounce the ball back and forth to hit bricks!
---
+++ breakout/breakout.py
@@ -0,0 +1,137 @@
+import numpy as np
+from math import cos, sin, degrees, radians, atan2
+from time import sleep
+from enum import Enum
+from asciimatics.screen import Screen
+from pynput.keyboard import Key, KeyCode, Listener
+
+class Direction(Enum):
+    NONE = 0
+    LEFT = -1
+    RIGHT = 1
+    
+QUIT_KEYS = (KeyCode.from_char("q"), KeyCode.from_char("Q"))
+DIRECTION_MAP = {
+  KeyCode.from_char("a"): Direction.LEFT,
+  KeyCode.from_char("d"): Direction.RIGHT,
+}
+HELD_KEY = None
+
+class Paddle():
+    def __init__(self, x, y, width):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.screen_width = (width * 6) - 1
+        self.direction = Direction.NONE
+        self.char_display = "▬" * width
+    
+    def move(self):
+        new_value = self.x + self.direction.value
+        if not(new_value < 1 or new_value + self.width > self.screen_width):
+            self.x = new_value
+
+    def __repr__(self):
+        return self.char_display
+
+class Ball():
+    def __init__(self, x, y, distance_per_step=0.4, angle=90):
+        self.x = x
+        self.y = y
+        self.distance_per_step = distance_per_step
+        self.angle = angle
+        self.char_display = "●"
+    
+    def get_next_step(self):
+        new_x = self.x + self.distance_per_step * cos(radians(self.angle))
+        new_y = self.y + self.distance_per_step * sin(radians(self.angle))
+        return new_x, new_y
+    
+    def move(self):
+        self.x, self.y = self.get_next_step()
+        if (self.y < 0):
+            self.angle = 90
+    
+    def __repr__(self):
+        return self.char_display
+
+class Brick():
+    def __init__(self, x, y, width):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.char_display = "■" * width
+    
+    def __repr__(self):
+        return self.char_display
+
+class BreakoutGame():
+    def __init__(self, width, height, n_bricks_col=8):
+        self.paddle = Paddle(x=(width // 2) - 2, y=height - 5, width=width // 6)
+        self.ball = Ball(x=width // 2, y=10)
+        
+        # Vertical walls to the left and right
+        # self.walls = 
+        
+        self.bricks = []
+        brick_width = width // n_bricks_col
+        for y in range(5):
+            for x in range(0, width, brick_width+1):
+                self.bricks.append(Brick(x, y, brick_width))
+    
+    def step(self):
+        self.paddle.move()
+        next_x, next_y = self.ball.get_next_step()
+        if ((next_x >= self.paddle.x and next_x <= self.paddle.x + self.paddle.width) and
+           (next_y >= self.paddle.y and next_y <= self.paddle.y + 1)):
+           self.ball.angle = degrees(
+               atan2(self.ball.y - (self.paddle.y + 0.5), self.ball.x - (self.paddle.x + (self.paddle.width // 2)))
+               # atan2(self.ball.y, self.ball.x) - atan2(self.paddle.y + 0.5, self.paddle.x + (self.paddle.width // 2))
+           )
+           print(self.ball.angle)
+        self.ball.move()
+    
+    def print_to_screen(self, screen):
+        screen.print_at(self.paddle, self.paddle.x, self.paddle.y)
+        for brick in self.bricks:
+            screen.print_at(brick, brick.x, brick.y)
+        screen.print_at(self.ball, int(round(self.ball.x)), int(round(self.ball.y)))
+
+def on_press(key):
+    global HELD_KEY
+    HELD_KEY = key
+
+def on_release(key):
+    global HELD_KEY
+    HELD_KEY = None
+
+def main(screen):
+    game = BreakoutGame(screen.width, screen.height)
+    listener = Listener(
+        on_press=on_press,
+        on_release=on_release
+    )
+    listener.start()
+    while True:
+        screen.clear_buffer(1, 1, 0)
+        
+        # screen.print_at(game.paddle, game.paddle.x, screen.height - 5)
+        game.step()
+        game.print_to_screen(screen)
+        
+        # While key inputs are being handled by pynput already, we still need
+        # to call get_key() in order to prevent inputs being written to the
+        # console once the game ends
+        screen.get_key()
+        if HELD_KEY in DIRECTION_MAP:
+            game.paddle.direction = DIRECTION_MAP[HELD_KEY]
+        elif HELD_KEY in QUIT_KEYS:
+            return
+        else:
+            game.paddle.direction = Direction.NONE
+        
+        screen.refresh()
+        sleep(0.02)
+
+if __name__ == "__main__":
+    Screen.wrapper(main)
